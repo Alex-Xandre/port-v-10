5215280145fa76cc5c71bda2c09e39548ecd8f18
Alex-Xandre <micua.161472@vigan.sti.edu.ph>
2026-08-03 09:04:05 +0800

    image: added new banner for matterly

--- src/app/(features)/projects/project-data.ts
+++ src/app/(features)/projects/project-data.ts
@@ -15,6 +15,7 @@ export const PROJECT_DATA: ProjectEntry[] = [
   {
     title: "Matterly",
     banner: [
+      "https://res.cloudinary.com/dfhhkd04c/image/upload/v1785718990/materly2_qru3b5.png",
       "https://res.cloudinary.com/dfhhkd04c/image/upload/v1785585019/materly_b5kt06.png",
     ],
     web_link: "",
